1538c844d5817466fe14c8eeb125fa26ef7fd8c2
yarden hovav <hovav4321@gmail.com>
2021-11-17 15:06:40 +0200

    creating a edit student`s details screen page

---
+++ app/src/main/java/com/example/studentsapp/EditStudent.java
@@ -0,0 +1,14 @@
+package com.example.studentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_student);
+    }
+}
