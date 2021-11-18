5c2e31c163ce656d83a7c4a4b01bab6fd53adb42
yarden hovav <hovav4321@gmail.com>
2021-11-18 19:26:06 +0200

    fixing the cb listener

--- app/src/main/java/com/example/studentsapp/StudentRecyclerActivity.java
+++ app/src/main/java/com/example/studentsapp/StudentRecyclerActivity.java
@@ -59,21 +59,33 @@ public class StudentRecyclerActivity extends AppCompatActivity {
             }
         });
 
+        adapter.setOnCbClickListener(new OnCbClickListener() {
+            @Override
+            public void OnCbClick(int position) {
+                Student student = data.get(position);
+                Log.d("TAG", "the cb:" +!student.getcb());
+                student.setCB(!student.getcb());
+                Log.d("TAG", "checkBox was clicked "+ position);
+            }
+        });
+
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
         private final OnItemClickListener listener;
+        private final OnCbClickListener cbListener;
         //תפקידו לזכור את הרפרנסים
         TextView name;
         TextView id;
         CheckBox cb;
-        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener, OnCbClickListener cbListener) {
             super(itemView);
 
             name = itemView.findViewById(R.id.list_row_name_tv);
             id = itemView.findViewById(R.id.list_row_id_tv);
             cb = itemView.findViewById(R.id.list_rpw_checkBox);
             this.listener = listener;
+            this.cbListener = cbListener;
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -87,8 +99,7 @@ public class StudentRecyclerActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View v) {
                     int pos = getAdapterPosition();
-                    //צריך לעדכן את הdata של הסטודנט
-                    Log.d("TAG", "checkBox was clicked "+ pos);
+                    cbListener.OnCbClick(pos);
                 }
             });
         }
@@ -104,12 +115,20 @@ public class StudentRecyclerActivity extends AppCompatActivity {
         void OnItemClick(int position);
     }
 
+    public interface OnCbClickListener{
+        void OnCbClick(int position);
+    }
+
     public class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
 
         private OnItemClickListener listener;
-
+        private OnCbClickListener cbListener;
         void setOnItemClickListener(OnItemClickListener listener){
             this.listener = listener;
+        }
+
+        void setOnCbClickListener(OnCbClickListener cbListener){
+            this.cbListener = cbListener;
         }
         @NonNull
         @Override
@@ -118,7 +137,7 @@ public class StudentRecyclerActivity extends AppCompatActivity {
             LayoutInflater inflater = getLayoutInflater();
             View rowView = inflater.inflate(R.layout.student_list_row, parent,false);
             //יצירת הולדר שיעטוף אותו
-            MyViewHolder viewHolder = new MyViewHolder(rowView, listener);
+            MyViewHolder viewHolder = new MyViewHolder(rowView, listener, cbListener);
             return viewHolder;
         }
 
